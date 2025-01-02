1f6a93964bbeb6053d34bede8bd9c204c111c6b3
Fernando Mota <83914329+fernandoMota93@users.noreply.github.com>
2025-01-02 08:49:05 -0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -6,12 +6,12 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.2/firebas
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDrFXEmGJSMsjnDxKPGW6cnpmvwICyOrnE",
-  authDomain: "gambette-25ba8.firebaseapp.com",
-  projectId: "gambette-25ba8",
-  storageBucket: "gambette-25ba8.firebasestorage.app",
-  messagingSenderId: "567036424540",
-  appId: "1:567036424540:web:0578e760cc246f4f069199"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
